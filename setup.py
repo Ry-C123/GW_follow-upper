2974ac8a587e14410003e3ecbeb3493e9979ca03
Ry <22173649+ryanc123@users.noreply.github.com>
2019-10-14 17:31:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='smcover',
       version='0.1',
-      description='',
+      description='Determines the area, and probability of follow up campaigns for GW triggers',
       url= 'https://github.com/ryanc123/GW_follow-upper' ,
       author='Ry Cutter',
       author_email='R.Cutter@warwick.ac.uk',
