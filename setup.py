d34872da9542c5a1c7dc01e40e5c2b6d392329dc
Ry <22173649+ryanc123@users.noreply.github.com>
2019-10-14 17:29:53 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='smcover',
+      version='0.1',
+      description='',
+      url= '' ,
+      author='Ry Cutter',
+      author_email='R.Cutter@warwick.ac.uk',
+      license='GNU',
+      packages=['SMC'],
+      include_package_data=True,
+      zip_safe=False)
